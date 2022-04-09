c974e039ed0b651a949d79722c161934e6f6547a
Yihui Xiong <yihui.xiong@hotmail.com>
2022-04-09 11:22:17 +0800

    use supervisor.usb_connected if available

--- keyboard/util.py
+++ keyboard/util.py
@@ -1,11 +1,15 @@
 
-@micropython.asm_thumb
-def mem(r0):
-    """Read memory from the address"""
-    ldr(r0, [r0, 0])
+import supervisor
 
 
 def usb_is_connected():
+    if hasattr(supervisor.runtime, 'usb_connected'):
+        return supervisor.usb_connected
+
+    @micropython.asm_thumb
+    def mem(r0):
+        """Read memory from the address"""
+        ldr(r0, [r0, 0])
     return mem(0x40000438) == 0x3
 
 
