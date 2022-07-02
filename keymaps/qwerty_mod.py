1d12a40ec98d5e75252ce808db8cb7c232c9613d
hyx0329 <hyx0329@163.com>
2022-07-02 22:09:42 +0800

    use CAPS as ESC(tap) and LCTRL(hold)
    Signed-off-by: hyx0329 <hyx0329@163.com>

--- keymaps/qwerty_mod.py
+++ keymaps/qwerty_mod.py
@@ -3,12 +3,13 @@ from keyboard.action_code import *
 from .default import kb_layer2_media, kb_layer3_kbcontrol, kb_layer4_restore, kb_layer5_mouse
 from .default import ___, FNL1, L2D, L3B, LSFT4, RSFT4, L5S, SCC, BOOT
 
+CESC = MODS_TAP(MODS(LCTRL), ESC)
 
 # layer 0
 kb_layer0_base = (
 	ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, '-', '=', BACKSPACE,
 	TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, '[', ']', '|',
-	CAPS,  A, L5S, L2D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
+	CESC,  A, L5S, L2D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
 	LSFT4, Z,   X,   C,   V, L3B,   N,   M, ',', '.', '/',         RSFT4,
 	LCTRL, LALT, LGUI,          SPACE,            RGUI, RALT, FNL1, RCTRL
 )
@@ -32,7 +33,7 @@ kb_layer0_base_plain = (
 kb_layer1_function = (
 	'`',  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
 	___, ___,  UP, ___, ___, ___, ___, ___, ___, SUSPEND,PAUSE,AUDIO_VOL_DOWN,AUDIO_VOL_UP,AUDIO_MUTE,
-	___,LEFT,DOWN,RIGHT,___, ___, HEATMAP, ___, ___, ___, ___, ___, MACRO(0),
+	CAPS,LEFT,DOWN,RIGHT,___, ___, HEATMAP, ___, ___, ___, ___, ___, MACRO(0),
 	___, MACRO(1), MACRO(2), MACRO(3), MACRO(4), BOOT, ___, ___, ___, ___, ___, ___,
 	___, ___, ___,                ___,               PRTSCN, ___, ___,  ___
 )
